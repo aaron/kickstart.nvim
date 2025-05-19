ff3b2ef3cade81fa0c3a292561b025d8cd2b6ee7
Aaron Baldwin <baldwina@brightwayslearning.org>
2025-05-19 17:00:10 -0600

    enable spell checking

--- init.lua
+++ init.lua
@@ -166,6 +166,10 @@ vim.o.scrolloff = 4
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Enable spell checking
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_us' }
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
